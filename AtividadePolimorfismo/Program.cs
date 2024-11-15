87823dab0ebbeb23055f8dafbbacdb35e5f049cb
MaluAbrami <marialuizaabrami@gmail.com>
2024-11-15 18:31:19 -0300

    add base para o programa principal e seu menu

--- AtividadePolimorfismo/Program.cs
+++ AtividadePolimorfismo/Program.cs
@@ -10,6 +10,36 @@ namespace AtividadePolimorfismo
     {
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("-------------Menu-------------");
+            Console.WriteLine("1 - Incluir um dos produtos na cesta\n2 - Reajustar o preço de todos os produtos\n3 - Exibir cesta de produtos\n4 - Exibir total da compra");
+            Console.WriteLine("Selecione uma opção: ");
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
         }
     }
 }
